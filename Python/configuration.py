Add code to debug quaternion rotations. Fix quaternion rotation for ground truth and stack trajectory quaternions in the right order.

Python/configuration.py
@@ -37,7 +37,9 @@ class SensorConfiguration:
             self.orientation[1])
         q_z = quat_from_axis_angle( np.array([0.0, 0.0, 1.0]), \
             self.orientation[2])
-        q = q_z * q_y * q_x
+
+        # Equation 3.1d
+        q = q_x * q_y * q_z
         return q
 
     def get_orientation_quat_array(self, n: int):

Python/create_ground_truth.py
@@ -6,8 +6,10 @@ import quaternion as quat
 
 from configuration import SensorConfiguration
 from data_structures import DataSeries, Trajectory
-from utilities import closest_point, quaternion_from_axis_angle, \
-    quaternion_to_vec3, quaternion_to_vec4, vec4_to_quaternion
+from utilities import closest_point, quat_from_axis_angle, \
+    vec3_to_quat, vec4_to_quat, \
+    vec3_array_to_quat_array, vec4_array_to_quat_array, \
+    quat_array_to_vec3_array, quat_array_to_vec4_array
 
 def format_navigation_series(data: Dict):
     aps_timestamps = data["APS"]["Epoch"].to_numpy()
@@ -19,6 +21,8 @@ def format_navigation_series(data: Dict):
         data["Gyroscope"]["Pitch"], data["Gyroscope"]["Heading"] ]).T
 
     gyro_series = gyro_series * np.pi / 180
+
+    print(gyro_series[:10])
 
     aps = DataSeries(aps_timestamps, aps_series)
     gyro = DataSeries(gyro_timestamps, gyro_series)
@@ -32,51 +36,64 @@ def estimate_camera_trajectory(aps: DataSeries, gyro: DataSeries, \
     K = len(aps.timestamps)
 
     # Allocate.
-    cam_attitudes = np.zeros((K, 4))
-    cam_timestamps = np.zeros(K)
-    cam_attitudes = vec4_to_quaternion(cam_attitudes)
-    
+    timestamps = np.zeros(K)
+    world_translations_camera= vec4_array_to_quat_array(np.zeros((K, 4)))
+    world_rotations_camera = vec4_array_to_quat_array(np.zeros((K, 4)))
+
+    world_translations_body = vec4_array_to_quat_array(np.zeros((K, 4)))
+    world_rotations_body =  vec4_array_to_quat_array(np.zeros((K, 4)))    
+
     # Get sensor configuration as quaternion arrays.
-    cam_translations = sensor.get_translation_quat_array(K)
-    cam_orientations = sensor.get_orientation_quat_array(K)
+    body_translations_camera = sensor.get_translation_quat_array(K)
+    body_rotations_camera = sensor.get_orientation_quat_array(K)
 
     # For every APS measurement, estimate the camera position.
     for k in range(K):
         aps_timestamp = aps.timestamps[k]
         aps_position = aps.series[k]
 
-        cam_timestamps[k] = aps_timestamp
+        timestamps[k] = aps_timestamp
 
         j, _ = closest_point(aps_timestamp, gyro.timestamps)
         gyro_timestamp = gyro.timestamps[j]
         gyro_attitude = gyro.series[j]
 
-        q_roll = quaternion_from_axis_angle(np.array([ 1.0, 0.0, 0.0 ]), \
+        # Create roll, pitch and yaw quaternions.
+        q_roll = quat_from_axis_angle(np.array([ 1.0, 0.0, 0.0 ]), \
             gyro_attitude[0])
-        q_pitch = quaternion_from_axis_angle(np.array([ 0.0, 1.0, 0.0 ]), \
+        q_pitch = quat_from_axis_angle(np.array([ 0.0, 1.0, 0.0 ]), \
             gyro_attitude[1])
-        q_yaw = quaternion_from_axis_angle(np.array([ 0.0, 0.0, 1.0 ]), \
+        q_yaw = quat_from_axis_angle(np.array([ 0.0, 0.0, 1.0 ]), \
             gyro_attitude[2])
 
-        q_body = q_roll * q_pitch * q_yaw
+        world_translations_body[k] = vec3_to_quat(aps_position)
+        world_rotations_body[k] =  q_yaw * q_pitch * q_roll
 
-        # Rotate lever arms and direction vectors.
-        cam_translations[k]  = q_body * cam_translations[k] * q_body.conjugate()
-        cam_attitudes[k] = cam_orientations[k] * q_body
+        # Translation / position.
+        world_translations_camera[k] = \
+             world_rotations_body[k] * body_translations_camera[k] \
+             * world_rotations_body[k].conjugate() + world_translations_body[k]
+
+        # Rotation.
+        world_rotations_camera[k] = \
+            world_rotations_body[k] * body_rotations_camera[k]
         
-    cam_translations = quaternion_to_vec3(cam_translations)
-    cam_positions = aps.series + cam_translations
-    cam_attitudes = quaternion_to_vec4(cam_attitudes)
+    camera_positions = quat_array_to_vec3_array(world_translations_camera)
+    camera_attitudes = quat_array_to_vec4_array(world_rotations_camera)
 
-    cam_trajectory = Trajectory(cam_timestamps, cam_positions, cam_attitudes)
+    camera_trajectory = Trajectory(timestamps, camera_positions, \
+        camera_attitudes)
 
-    return cam_trajectory
+    return camera_trajectory
 
 
 def main():
-    # Sensor configuration
+    declination = 40.0 # 48.0
     camera_translation = np.array([ 2.00, 0.21, 1.40 ])
-    camera_orientation = np.array([ 0.00, 48.0, 0.00 ]) * np.pi / 180
+    camera_orientation = np.array([ 0.00, 90.0 - declination, 90.0 ]) \
+        * np.pi / 180
+
+    # Sensor configuration.
     sensor_config = SensorConfiguration(camera_translation, camera_orientation)
 
     data = {}
@@ -93,7 +110,7 @@ def main():
 
     # Write csv.
     reference.save_as_csv(\
-        "/home/martin/dev/Trajectory/Output/Groundtruth-Dive-02.csv")
+        "/home/martin/dev/Trajectory/Output/Ground-Truth-Dive-02.csv")
 
 if __name__ == "__main__":
     main()

Python/debug-intrinsic-rotation.py
@@ -9,59 +9,95 @@ import quaternion as quat
 from utilities import quat_from_axis_angle, vec3_to_quat, quat_to_vec3
 
 def main():
+
+    # -------------------------------------------------------------------------
+    # ---- Camera -------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    
+    # Rotations.
     alpha_x = 0.0
     alpha_y = 90.0 - 48.0
     alpha_z = 90.0
 
-    q_x = quat_from_axis_angle(np.array([ 1.0, 0.0, 0.0]), \
+    # Translation.
+    b_t_c = np.array([ 2.0, 0.21, 1.40 ])
+
+    q_x = quat_from_axis_angle(np.array([ 1.0, 0.0, 0.0 ]), \
         alpha_x  * np.pi / 180.0)
-    q_y = quat_from_axis_angle(np.array([ 0.0, 1.0, 0.0]), \
+    q_y = quat_from_axis_angle(np.array([ 0.0, 1.0, 0.0 ]), \
         alpha_y  * np.pi / 180.0)
-    q_z = quat_from_axis_angle(np.array([ 0.0, 0.0, 1.0]), \
+    q_z = quat_from_axis_angle(np.array([ 0.0, 0.0, 1.0 ]), \
         alpha_z * np.pi / 180.0)
 
-    q_bc = q_x * q_y * q_z
+    b_q_c = q_x * q_y * q_z
 
-    #q_bc = q_cb.conjugate() / ( np.norm(q_cb) * np.norm(q_cb) )
+    # Origin and unit vectors in camera coordinate system.
+    c_p_c = np.array([ 0.0, 0.0, 0.0 ])
+    c_u_x = np.array([ 1.0, 0.0, 0.0 ])
+    c_u_y = np.array([ 0.0, 1.0, 0.0 ])
+    c_u_z = np.array([ 0.0, 0.0, 1.0 ])
+    c_u_x = vec3_to_quat(c_u_x)
+    c_u_y = vec3_to_quat(c_u_y)
+    c_u_z = vec3_to_quat(c_u_z)
 
-    # Point in camera coordinate system.
-    p_c = np.array([ 1.0, 2.0, 3.0 ])
-    v_cx = np.array([ 1.0, 0.0, 0.0 ])
-    v_cy = np.array([ 0.0, 1.0, 0.0 ])
-    v_cz = np.array([ 0.0, 0.0, 1.0 ])
+    # Translate and rotate from camera to body.
+    b_p_c = c_p_c + b_t_c
+    b_u_x = b_q_c * c_u_x * b_q_c.conjugate()
+    b_u_y = b_q_c * c_u_y * b_q_c.conjugate()
+    b_u_z = b_q_c * c_u_z * b_q_c.conjugate()
 
-    # Camera coordinate system unit vectors.
-    v_cx = vec3_to_quat(v_cx)
-    v_cy = vec3_to_quat(v_cy)
-    v_cz = vec3_to_quat(v_cz)
-    p_c = vec3_to_quat(p_c)
+    # -------------------------------------------------------------------------
+    # ---- Body ---------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    
+    # Body frame orientation.
+    b_t = np.array([ 1.0, 2.0, 3.0 ])
+    roll = 20
+    pitch = 30
+    yaw = 145
 
-    # Rotate from camera to body.
-    v_bx = q_bc * v_cx * q_bc.conjugate()
-    v_by = q_bc * v_cy * q_bc.conjugate()
-    v_bz = q_bc * v_cz * q_bc.conjugate()
-    p_b = q_bc * p_c * q_bc.conjugate()
+    q_roll = quat_from_axis_angle(np.array([ 1.0, 0.0, 0.0 ]), \
+        roll * np.pi / 180.0)
+    q_pitch = quat_from_axis_angle(np.array([ 0.0, 1.0, 0.0 ]), \
+        pitch * np.pi / 180.0)
+    q_yaw = quat_from_axis_angle(np.array([ 0.0, 0.0, 1.0 ]), \
+        yaw * np.pi / 180.0)
 
-    # Extract vectors.
-    v_bx = quat_to_vec3(v_bx)
-    v_by = quat_to_vec3(v_by)
-    v_bz = quat_to_vec3(v_bz)
-    p_b = quat_to_vec3(p_b)
+    w_q_b = q_roll * q_pitch * q_yaw
 
-    print("Unit vector x: {0}".format(v_bx))
-    print("Unit vector y: {0}".format(v_by))
-    print("Unit vector z: {0}".format(v_bz))
-    print("Point:         {0}".format(p_b))
+    # Origin and unit vectors in camera coordinate system.
+    b_origin = np.array([ 0.0, 0.0, 0.0 ])
+    b_v_x = np.array([ 1.0, 0.0, 0.0 ])
+    b_v_y = np.array([ 0.0, 1.0, 0.0 ])
+    b_v_z = np.array([ 0.0, 0.0, 1.0 ])
 
-    fig1, ax1 = plt.subplots(figsize=(8, 8))
+    """
+    b_origin = vec3_to_quat(c_p)
+    b_u_x = vec3_to_quat(c_u_x)
+    b_u_y = vec3_to_quat(c_u_y)
+    b_u_z = vec3_to_quat(c_u_z)
+    """
+    b_u_x = quat_to_vec3(b_u_x)
+    b_u_y = quat_to_vec3(b_u_y)
+    b_u_z = quat_to_vec3(b_u_z)
+
+    fig1, ax1 = plt.subplots(figsize=(8, 5))
     ax1 = fig1.add_subplot(111, projection='3d')
-    ax1.quiver(0, 0, 0, 1, 0, 0, color="y", label="Body")
-    ax1.quiver(0, 0, 0, 0, 1, 0, color="y")
-    ax1.quiver(0, 0, 0, 0, 0, 1, color="y")
-    ax1.quiver(0, 0, 0, v_bx[0], v_bx[1], v_bx[2], color="r", label=r"$x_{c}$")
-    ax1.quiver(0, 0, 0, v_by[0], v_by[1], v_by[2], color="g", label=r"$y_{c}$")
-    ax1.quiver(0, 0, 0, v_bz[0], v_bz[1], v_bz[2], color="b", label=r"$z_{c}$")
-    ax1.scatter(p_b[0], p_b[1], p_b[2], label="Point")
+
+    ax1.quiver(b_o_b[0], b_o_b[1], b_o_b[2], b_v_x[0], b_v_x[1], b_v_x[2], \
+        color="r")
+    ax1.quiver(b_o_b[0], b_o_b[1], b_o_b[2], b_v_y[0], b_v_y[1], b_v_y[2], \
+        color="g")
+    ax1.quiver(b_o_b[0], b_o_b[1], b_o_b[2], b_v_z[0], b_v_z[1], b_v_z[2], \
+        color="b")
+
+    ax1.quiver(b_p_c[0], b_p_c[1], b_p_c[2], b_u_x[0], b_u_x[1], b_u_x[2], \
+        color="r")
+    ax1.quiver(b_p_c[0], b_p_c[1], b_p_c[2], b_u_y[0], b_u_y[1], b_u_z[2], \
+        color="g")
+    ax1.quiver(b_p_c[0], b_p_c[1], b_p_c[2], b_u_z[0], b_u_z[1], b_u_z[2], \
+        color="b")
+
     ax1.set_xlabel(r"$x_{b}$")
     ax1.set_ylabel(r"$y_{b}$")
     ax1.set_zlabel(r"$z_{b}$")
@@ -69,8 +105,8 @@ def main():
     ax1.set_xlim([-5, 5])
     ax1.set_ylim([-5, 5])
     ax1.set_zlim([-5, 5])
-    ax1.legend()
 
     plt.show()
+
 if __name__ == "__main__":
     main()

Python/georeference.py
@@ -16,14 +16,16 @@ import optimization
 from configuration import Configuration
 from data_structures import Map, MapUnpacker, Trajectory
 from plotting import plot_3D_line, plot_3D_scatter
-from utilities import quat_from_axis_angle, quat_array_to_vec3_array, \
-    vec4_array_to_quat_array
+from utilities import closest_point, clamp_signal, quat_from_axis_angle, \
+    quat_array_to_vec3_array, vec4_array_to_quat_array
 
 def visualize_results(trajectories: Dict, config: Configuration):
     # Create variables.
     ground_truth = trajectories["Ground-Truth"]
     keyframes = trajectories["Keyframes"]
     frames = trajectories["Frames"]
+    matched_ground_truth = trajectories["Matched-Ground-Truth"]
+    matched_keyframes = trajectories["Matched-Keyframes"]
 
     # Plot parameters.
     plot_margin_position = 2
@@ -66,56 +68,63 @@ def visualize_results(trajectories: Dict, config: Configuration):
     ax3[2].set_ylabel(r"Depth, $D$ $[m]$")
     ax3[2].legend(loc="lower right")
 
-    
     # Get ground truth attitude.
-    q_wc = vec4_array_to_quat_array(ground_truth.attitudes)
-    q_wc = vec4_array_to_quat_array(keyframes.attitudes)
+    quat_ground_truth = vec4_array_to_quat_array(ground_truth.attitudes)
+    quat_keyframes = vec4_array_to_quat_array(keyframes.attitudes)
 
-    q_cb = quat_from_axis_angle()
+    angles_ground_truth = quat.as_rotation_vector(quat_ground_truth)
+    angles_keyframes = quat.as_rotation_vector(quat_keyframes)
+
+    angles_ground_truth *= 180 / np.pi
+    angles_keyframes *= 180 / np.pi
+
+    angles_ground_truth = clamp_signal(angles_ground_truth, 0, 360)
+    angles_keyframes = clamp_signal(angles_keyframes, 0, 360)
 
     # Visualize attitudes - Figure.
     fig4, ax4 = plt.subplots(nrows=3, ncols=1, figsize=(8, 5))
     fig4.tight_layout(h_pad=2)
 
-    # Visualize attitudes - Roll.
-    ax4[0].plot(keyframes.timestamps, q_bc[:, 0], \
-        label="BC")
-    ax4[0].plot(ground_truth.timestamps, q_bw[:, 0], \
-        label="BW")
-    ax4[0].plot(ground_truth.timestamps, q_bw[:, 0], \
-        label="BW")
+    ax4[0].plot(keyframes.timestamps, angles_keyframes[:, 0], \
+        label="Keyframes")
+    ax4[0].plot(ground_truth.timestamps, angles_ground_truth[:, 0], \
+        label="Ground truth")
     ax4[0].set_xlim([ keyframes.timestamps[0], keyframes.timestamps[-1] ])
+    """
     ax4[0].set_ylim([ \
         np.min(attitudes_keyframes[:, 0]) - plot_margin_attitude, \
         np.max(attitudes_keyframes[:, 0]) + plot_margin_attitude ])
+    """
     ax4[0].set_xlabel(r"Time, $t$ $[s]$")
-    ax4[0].set_ylabel(r"Roll, $r_{x}$ $[-]$")
+    ax4[0].set_ylabel(r"Euler X, $r_{x}$ $[-]$")
     ax4[0].legend(loc="lower right")
 
-    # Visualize attitudes - Pitch.
-    ax4[1].plot(keyframes.timestamps, attitudes_keyframes[:, 1], \
+    ax4[1].plot(keyframes.timestamps, angles_keyframes[:, 1], \
         label="Keyframes")
-    ax4[1].plot(ground_truth.timestamps, attitudes_gt[:, 1], \
+    ax4[1].plot(ground_truth.timestamps, angles_ground_truth[:, 1], \
         label="Ground truth")
     ax4[1].set_xlim([ keyframes.timestamps[0], keyframes.timestamps[-1] ])
+    """
     ax4[1].set_ylim([ \
         np.min(attitudes_keyframes[:, 1]) - plot_margin_position, \
         np.max(attitudes_keyframes[:, 1]) + plot_margin_position ])
+    """
     ax4[1].set_xlabel(r"Time, $t$ $[s]$")
-    ax4[1].set_ylabel(r"Pitch, $r_{y}$ $[-]$")
+    ax4[1].set_ylabel(r"Euler Y, $r_{y}$ $[-]$")
     ax4[1].legend(loc="lower right")
 
-    # Visualize attitude - Heading.
-    ax4[2].plot(keyframes.timestamps, attitudes_keyframes[:, 2], \
+    ax4[2].plot(keyframes.timestamps, angles_keyframes[:, 2], \
         label="Keyframes")
-    ax4[2].plot(ground_truth.timestamps, attitudes_gt[:, 2], \
+    ax4[2].plot(ground_truth.timestamps, angles_ground_truth[:, 2], \
         label="Ground truth")
     ax4[2].set_xlim([ keyframes.timestamps[0], keyframes.timestamps[-1] ])
+    """
     ax4[2].set_ylim([ \
         np.min(attitudes_keyframes[:, 2]) - plot_margin_position, \
         np.max(attitudes_keyframes[:, 2]) + plot_margin_position ])
+    """
     ax4[2].set_xlabel(r"Time, $t$ $[s]$")
-    ax4[2].set_ylabel(r"Heading, $r_{z}$ $[-]$")
+    ax4[2].set_ylabel(r"Euler Z, $r_{z}$ $[-]$")
     ax4[2].legend(loc="lower right")
 
     # Visualize matched window.
@@ -153,9 +162,9 @@ def georeference(trajectories: Dict, map: Map, config: Configuration):
     matched_keyframes = results.matched_keyframes
     matched_ground_truth = results.matched_ground_truth
 
-    # Add original keyframe trajectory.
-    trajectories["Original"] = copy.deepcopy(keyframes)
-
+    # Add matched trajectories.
+    trajectories["Matched-Keyframes"] = matched_keyframes
+    trajectories["Matched-Ground-Truth"] = matched_ground_truth
 
     # Add bias and apply rotation and translation.
     keyframes.add_time_bias(config.optim.bias)
@@ -164,16 +173,51 @@ def georeference(trajectories: Dict, map: Map, config: Configuration):
     frames.apply_SE3_transform(rotation, translation)
     landmarks.apply_SE3_transform(rotation, translation)
 
-    # Printout.
-    print("Rotation, vector: {0}".format( \
-        quat.as_rotation_vector(rotation) * 180 / np.pi))
-    print("Rotation, angles: {0}".format( \
-        quat.as_euler_angles(rotation) * 180 / np.pi))
-    print("Translation: {0}".format(translation))
-    print("Time interval: {0}, {1}".format(keyframes.timestamps[0], \
-        keyframes.timestamps[-1]))
-
     visualize_results(trajectories, config)
+
+    # Temporary.
+    start, _ = closest_point(keyframes.timestamps[0], ground_truth.timestamps)
+    end, _ = closest_point(keyframes.timestamps[-1], ground_truth.timestamps)
+
+    truncated = Trajectory(ground_truth.timestamps[start:end], \
+        ground_truth.positions[start:end], ground_truth.attitudes[start:end])
+
+    kf_directions = np.zeros(( len(keyframes.positions), 4 ))
+    gt_directions = np.zeros(( len(truncated.positions), 4 ))
+    kf_directions[:, 3] = 1.0
+    gt_directions[:, 3] = 1.0
+    kf_directions = vec4_array_to_quat_array(kf_directions)
+    gt_directions = vec4_array_to_quat_array(gt_directions)
+
+    kf_quats = vec4_array_to_quat_array(keyframes.attitudes)
+    gt_quats = vec4_array_to_quat_array(truncated.attitudes)
+
+    # Rotate directions.
+    kf_directions = kf_quats * kf_directions * kf_quats.conjugate()
+    gt_directions = gt_quats * gt_directions * gt_quats.conjugate()
+    kf_directions = quat_array_to_vec3_array(kf_directions)
+    gt_directions = quat_array_to_vec3_array(gt_directions)
+
+    fig, ax = plot_3D_line(keyframes.positions[:, 0], \
+        keyframes.positions[:, 1], keyframes.positions[:, 2], \
+        equal_axes=True, color="b", label="Keyframes")
+
+    ax.plot(truncated[:, 0], truncated[:, 1], truncated[:, 2], \
+        color="r", label="Ground truth")
+
+    ax.quiver(keyframes[:, 0], keyframes[:, 1], keyframes[:, 2], \
+        kf_directions[:, 0], kf_directions[:, 1], kf_directions[:, 2], \
+        color="b", length=0.4)
+
+    ax.quiver(truncated[:, 0], truncated[:, 1], truncated[:, 2], \
+        gt_directions[:, 0], gt_directions[:, 1], gt_directions[:, 2], \
+        color="r", length=0.4)
+
+    ax.legend()
+
+    
+
+    plt.show()
 
     if config.save_output:
         keyframes.save_as_csv(config.output_dir + config.keyframes_file)

Python/plotting.py
@@ -37,7 +37,7 @@ def set_axes_equal(ax):
     ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
 
-def plot_3D_line(x: np.array, y: np.array, z: np.array, \
+def plot_3D_line(x: np.array, y: np.array, z: np.array, color: str="b", \
     figsize: Tuple=(6, 6), label: str="", xlabel: str="", ylabel: str="", \
     zlabel: str="", title: str="", legend: bool=False, equal_axes: bool=False):
     """
@@ -46,7 +46,7 @@ def plot_3D_line(x: np.array, y: np.array, z: np.array, \
     """
     fig, ax = plt.subplots(figsize=figsize)
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot(x, y, z, label=label)
+    ax.plot(x, y, z, label=label, color=color)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_zlabel(zlabel)

Python/run_georeferencing.py
@@ -16,10 +16,20 @@ def format_trajectory(data: Dict, key: str):
     timestamps = np.array(data[key]["Timestamps"])
     positions = np.stack([ data[key]["PositionX"], data[key]["PositionY"], \
         data[key]["PositionZ"] ]).T
+    attitudes = np.stack([ data[key]["Quaternion4"], data[key]["Quaternion1"], \
+        data[key]["Quaternion2"], data[key]["Quaternion3"] ]).T
+
+    return Trajectory(timestamps, positions, attitudes)
+
+def format_ground_truth(data: Dict, key: str):
+    timestamps = np.array(data[key]["Timestamps"])
+    positions = np.stack([ data[key]["PositionX"], data[key]["PositionY"], \
+        data[key]["PositionZ"] ]).T
     attitudes = np.stack([ data[key]["Quaternion1"], data[key]["Quaternion2"], \
         data[key]["Quaternion3"], data[key]["Quaternion4"] ]).T
 
     return Trajectory(timestamps, positions, attitudes)
+   
 
 def format_data(args):
     # Optimization configuration.
@@ -37,7 +47,7 @@ def format_data(args):
     data["Keyframes"]   = pd.read_csv(args.slam + args.keyframes)
     data["Map"]         = read_msgpack(args.slam + args.map)
 
-    groundtruth = format_trajectory(data, "Ground-Truth")
+    groundtruth = format_ground_truth(data, "Ground-Truth")
     keyframes = format_trajectory(data, "Keyframes")
     frames = format_trajectory(data, "Frames")
     map = data["Map"].load()